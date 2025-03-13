d50e0fff8b283c6e056b6065843511a622886b09
Misbah Haque <misbahmuneer2003@gmail.com>
2025-03-13 12:02:01 +0500

    Update

--- lib/works.dart
+++ lib/works.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:video_player/video_player.dart';
 import 'package:portfolio/WidgetSupport.dart';
 import 'package:portfolio/landingPage.dart';
@@ -12,22 +11,32 @@ class Works extends StatefulWidget {
 }
 
 class _WorksState extends State<Works> {
-  late VideoPlayerController _videoController;
+  late VideoPlayerController _cyberfictionController;
+  late VideoPlayerController _zeltController;
 
   @override
   void initState() {
     super.initState();
-    _videoController = VideoPlayerController.asset("assets/videos/0303.mp4")
+    _cyberfictionController = VideoPlayerController.asset(
+        "assets/videos/0303.mp4",
+      )
       ..initialize().then((_) {
-        _videoController.setLooping(true);
-        _videoController.play();
+        _cyberfictionController.setLooping(true);
+        _cyberfictionController.play();
+        setState(() {});
+      });
+    _zeltController = VideoPlayerController.asset("assets/videos/0307.mp4")
+      ..initialize().then((_) {
+        _zeltController.setLooping(true);
+        _zeltController.play();
         setState(() {});
       });
   }
 
   @override
   void dispose() {
-    _videoController.dispose();
+    _cyberfictionController.dispose();
+    _zeltController.dispose();
     super.dispose();
   }
 
@@ -36,75 +45,111 @@ class _WorksState extends State<Works> {
     double screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 30, top: 30, right: 30),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => LandingPage(),
-                          ),
-                        );
-                      },
-                      child: Text("M — H", style: AppWidget.BigStyle()),
-                    ),
-                    GestureDetector(
-                      onTap: () {},
-                      child: Text("MENU", style: AppWidget.BigStyle()),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 40),
-              Row(
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 30, top: 30, right: 30),
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 100),
-                    child: Row(
-                      children: [
-                        Text("01", style: AppWidget.NavStyle()),
-                        const SizedBox(width: 80),
-                        Text("CYBERFICTION", style: AppWidget.NavStyle()),
-                      ],
-                    ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => LandingPage()),
+                      );
+                    },
+                    child: Text("M — H", style: AppWidget.BigStyle()),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 150),
-                    child: Text("SEE PROJECT", style: AppWidget.NavStyle()),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Text("MENU", style: AppWidget.BigStyle()),
                   ),
                 ],
               ),
-              const SizedBox(height: 20),
-              Center(
-                child: Container(
-                  width: screenWidth * 0.8,
-                  height: 700,
-                  color: Colors.black,
-                  child:
-                      _videoController.value.isInitialized
-                          ? ClipRRect(
-                            borderRadius: BorderRadius.circular(10),
-                            child: AspectRatio(
-                              aspectRatio: _videoController.value.aspectRatio,
-                              child: VideoPlayer(_videoController),
-                            ),
-                          )
-                          : const Center(child: CircularProgressIndicator()),
+            ),
+            const SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 100),
+                  child: Row(
+                    children: [
+                      Text("01", style: AppWidget.NavStyle()),
+                      const SizedBox(width: 80),
+                      Text("CYBERFICTION", style: AppWidget.NavStyle()),
+                    ],
+                  ),
                 ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 150),
+                  child: Text("SEE PROJECT", style: AppWidget.NavStyle()),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Center(
+              child: Container(
+                width: screenWidth * 0.8,
+                height: 700,
+                color: Colors.black,
+                child:
+                    _cyberfictionController.value.isInitialized
+                        ? ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: AspectRatio(
+                            aspectRatio:
+                                _cyberfictionController.value.aspectRatio,
+                            child: VideoPlayer(_cyberfictionController),
+                          ),
+                        )
+                        : const Center(child: CircularProgressIndicator()),
               ),
-            ],
-          ),
-        ],
+            ),
+            const SizedBox(height: 200),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 100),
+                  child: Row(
+                    children: [
+                      Text("02", style: AppWidget.NavStyle()),
+                      const SizedBox(width: 80),
+                      Text("ZELT", style: AppWidget.NavStyle()),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 150),
+                  child: Text("SEE PROJECT", style: AppWidget.NavStyle()),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Center(
+              child: Container(
+                width: screenWidth * 0.8,
+                height: 700,
+                color: Colors.black,
+                child:
+                    _zeltController.value.isInitialized
+                        ? ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: AspectRatio(
+                            aspectRatio: _zeltController.value.aspectRatio,
+                            child: VideoPlayer(_zeltController),
+                          ),
+                        )
+                        : const Center(child: CircularProgressIndicator()),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
