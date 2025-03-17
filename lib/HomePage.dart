4a2cf0b555c8979f39dee1cac8c34a25dde04084
Misbah Haque <misbahmuneer2003@gmail.com>
2025-03-17 12:39:27 +0500

    Update

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -15,7 +15,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ResponsiveLayout(
-        desktopBody: LandingPage(),
+        desktopBody: SplashScreenPage(),
         mobileBody: MobileBody(),
       ),
     );
--- lib/responsive_Layout.dart
+++ lib/responsive_Layout.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:portfolio/TabletLayout/tabletLayout.dart';
 
 class ResponsiveLayout extends StatelessWidget {
   final Widget mobileBody;
@@ -13,8 +12,6 @@ class ResponsiveLayout extends StatelessWidget {
       builder: (context, constraints) {
         if (constraints.maxWidth < 600) {
           return mobileBody;
-        } else if (constraints.maxWidth > 600 && constraints.maxWidth < 1200) {
-          return TabletPage();
         } else {
           return desktopBody;
         }
